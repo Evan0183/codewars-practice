1d18522b2644b747fd3962fd714ffd5c8b4a4af5
Evan0183 <evan.henz@rocklinusd.org>
2025-01-15 23:23:49 +0000

    Began implementing making the number of decimal places in the check sum equal the maximum number of decimal places in any of the input numbers

--- 2024-prob-14/prob14.py
+++ 2024-prob-14/prob14.py
@@ -1,10 +1,18 @@
+def get_num_decimal_places(number):
+    decimal_places = 0
+
+    if "." in number:
+        decimal_places = len(number) - number.index(".") - 1
+
+    return decimal_places
+
 data = []
 check_sum = 0
 
 with open("2024-prob-14/input.txt") as file:
     lines = file.read().split("\n")
     data = lines[0].split(" ")
-    check_sum = float(lines[1][9:])
+    check_sum = lines[1][9:]
 
 numeric_data = []
 
@@ -14,6 +22,14 @@ for value in data:
         numeric_data.append(value)
     except ValueError:
         pass
+
+max_decimal_places = 0
+
+for value in numeric_data:
+    decimal_places = get_num_decimal_places(value)
+
+    if decimal_places > max_decimal_places:
+        max_decimal_places = decimal_places
 
 for i, value in enumerate(numeric_data):
     if i == len(numeric_data) - 1:
@@ -25,6 +41,13 @@ actual_sum = 0
 
 for value in numeric_data:
     actual_sum += float(value)
+
+actual_sum = str(actual_sum)
+sum_decimal_places = get_num_decimal_places(actual_sum)
+
+if sum_decimal_places < max_decimal_places:
+    difference = max_decimal_places - sum_decimal_places
+    actual_sum += "0" * difference
 
 if actual_sum == check_sum:
     print("CHECKED")
